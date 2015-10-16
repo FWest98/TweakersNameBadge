d00bec1859fc6ed3fa3491219bcb45a26c5578e2
Floris Westerman <floriswesterman@msn.com>
2015-10-16 15:14:16 +0200

    More improvements

---
+++ TweakersUserBadge/Helpers/AsyncLazy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TweakersUserBadge.Helpers {
+    public class AsyncLazy<T> : Lazy<Task<T>> {
+        public AsyncLazy(Func<T> valueFunc) : base(() => Task.Factory.StartNew(valueFunc)) { }
+        public AsyncLazy(Func<Task<T>> taskFunc) : base(() => Task.Factory.StartNew(taskFunc).Unwrap()) { }
+    }
+}
---
+++ TweakersUserBadge/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace TweakersUserBadge.Models {
+    public class User {
+        public string Karma { get; set; }
+        public string KarmaRanking { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Beroep { get; set; }
+        public string IconUrl { get; set; }
+        public string Tweakotine { get; set; }
+        public string Education { get; set; }
+    }
+}
